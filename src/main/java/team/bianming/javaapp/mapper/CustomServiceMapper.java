3a30460da0e5bd0d3ecf52e9ecec8f8249368320
phybrain <692114871@qq.com>
2017-08-28 22:34:09 +0800

    建立Test

---
+++ src/main/java/team/bianming/javaapp/mapper/CustomServiceMapper.java
@@ -0,0 +1,10 @@
+package team.bianming.javaapp.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by xiaopeng on 2017/8/28.
+ */
+@Mapper
+public interface CustomServiceMapper {
+}
