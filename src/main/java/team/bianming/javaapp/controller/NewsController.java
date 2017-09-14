4c5a4dd255101e4603eaf739c65583fdf1203a5f
phybrain <692114871@qq.com>
2017-09-14 20:00:49 +0800

    文件上传

--- src/main/java/team/bianming/javaapp/controller/NewsController.java
+++ src/main/java/team/bianming/javaapp/controller/NewsController.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import team.bianming.javaapp.entity.News;
 import team.bianming.javaapp.service.NewsService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.util.Map;
 
 /**
@@ -22,7 +26,7 @@ public class NewsController {
     @Autowired
     NewsService newsService;
     @ResponseBody
-    @RequestMapping()
+    @RequestMapping("/newslist")
     public Page<News> ListNews(int pageSize, int pageNum, int categoryId) {
 
         return newsService.findNewsByCategory(pageNum,pageSize,categoryId);
@@ -34,4 +38,26 @@ public class NewsController {
         return "index";
     }
 
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        if (!file.isEmpty()) {
+            try {
+                String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filePath));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            }
+            return "上传成功";
+        } else {
+            return "上传失败，因为文件是空的.";
+        }
+    }
 }
