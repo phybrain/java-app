6bd50fff0cf20dc37d1312cd982d31fc6b10db66
sifu <sifu@wacai.com>
2017-10-18 14:29:07 +0800

    dsfsf

--- src/main/java/team/bianming/javaapp/controller/CSController.java
+++ src/main/java/team/bianming/javaapp/controller/CSController.java
@@ -228,7 +228,13 @@ public class CSController {
 
         List<RecordInfo> res = new ArrayList<>();
         RecordInfo recordInfo = new RecordInfo();
-        recordInfo.setContent("");
+        recordInfo.setContent("xxee");
+        recordInfo.setSendtime(new Date());
+        recordInfo.setSenderType("cs");
+        recordInfo.setSenderId(1);
+        recordInfo.setAccepterId(1);
+        recordInfo.setId(1);
+        recordInfo.setSessionId(1);
         res.add(recordInfo);
         return res;
     }
